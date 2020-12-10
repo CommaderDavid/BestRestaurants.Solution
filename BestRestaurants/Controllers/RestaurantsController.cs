b7a071ab558acfecaf1d2c28105820bcac0e5478
David Boedigheimer <stealth259@gmail.com>
2020-12-10 15:34:58 -0800

    Add Cuisine and restaurant views and controllers.

---
+++ BestRestaurants/Controllers/RestaurantsController.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using BestRestaurants.Models;
+
+// How do i get the cuisine to show and hold the restaurants?
