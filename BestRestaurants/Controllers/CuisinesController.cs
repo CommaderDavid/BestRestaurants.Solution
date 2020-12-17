830fca7a140984632f5ea633fd8474d4ed878493
David Boedigheimer <stealth259@gmail.com>
2020-12-16 16:04:10 -0800

    Add index and create to cuisines.

--- BestRestaurants/Controllers/CuisinesController.cs
+++ BestRestaurants/Controllers/CuisinesController.cs
@@ -9,19 +9,30 @@ namespace BestRestaurants.Controllers
 {
     public class CuisinesController : Controller
     {
-         private readonly BestRestaurantsContext _db;
+        private readonly BestRestaurantsContext _db;
 
-         public CuisinesController(BestRestaurantsContext db)
-         {
-             _db = db;
-         }
+        public CuisinesController(BestRestaurantsContext db)
+        {
+            _db = db;
+        }
 
-         public ActionResult Index()
-         {
-            List<Cuisine> model = _db.Cuisines.Include(cuisine => cuisine.CuisineName).ToList();
+        public ActionResult Index()
+        {
+            List<Cuisine> model = _db.Cuisines.ToList();
             return View(model);
-         }
+        }
 
-         
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Cuisine cuisine)
+        {
+            _db.Cuisines.Add(cuisine);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
